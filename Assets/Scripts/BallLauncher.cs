07dd2a533f14a772aa81d8659b8d31f9dd8e5f2e
Son Xuan Hoang <136507369+SonXH@users.noreply.github.com>
2024-10-30 13:57:44 +0100

    Initial commit

---
+++ Assets/Scripts/BallLauncher.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLauncher : MonoBehaviour
+{
+
+    private Vector3 startPoint;
+    private Vector3 endPoint;
+
+    GameObject ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        } else if (Input.GetMouseButton(0))
+        {
+
+        } else if (Input.GetMouseButtonUp(0))
+        {
+
+        }
+    }
+
+    void Shoot()
+    {
+        //Instantiate(ball, )
+    }
+}
